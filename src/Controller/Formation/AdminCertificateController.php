77d018e02d0798f3b5d0143becf21ca0db48c7d1
FyrasBenSlimane <firas.benslimen@esprit.tn>
2026-04-19 11:48:38 +0100

    feat(formation): update formation and certificate controllers
    - Update AdminCertificateController
    - Update formation templates
    - Add trainer formation creation template

--- src/Controller/Formation/AdminCertificateController.php
+++ src/Controller/Formation/AdminCertificateController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller\Formation;
 
+use App\Repository\CertificateRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,8 +15,10 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 final class AdminCertificateController extends AbstractController
 {
     #[Route(name: 'app_admin_certificates_index', methods: ['GET'])]
-    public function index(): Response
+    public function index(CertificateRepository $certificateRepository): Response
     {
-        return $this->render('admin/certificates/index.html.twig');
+        return $this->render('admin/certificates/index.html.twig', [
+            'certificates' => $certificateRepository->findBy([], ['issuedAt' => 'DESC']),
+        ]);
     }
 }
