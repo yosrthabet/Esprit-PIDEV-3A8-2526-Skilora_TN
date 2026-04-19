22188dde9624c9e9430473c8f8fd5ff20dc34cc6
FyrasBenSlimane <firas.benslimen@esprit.tn>
2026-04-19 11:51:03 +0100

    chore: update config, dependencies and assets
    - Update Symfony config (doctrine, security, twig, mercure)
    - Update composer dependencies
    - Rebuild Tailwind CSS
    - Remove formation1/ leftover folder
    - Update .gitignore and phpunit config

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,7 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
+    Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
+    KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
